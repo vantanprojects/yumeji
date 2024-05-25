e32ffcc319f6edf43dfa305844aa72e608489497
柳橋莉空 <yagihashirikuu@yagihashirikuunoMacBook-Air.local>
2024-05-25 22:46:58 +0900

    watasi

--- project/code/yumeji/settings.py
+++ project/code/yumeji/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'yumeji.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django-db',
+        'USER': 'django',
+        'PASSWORD': 'django',
+        'HOST': 'db',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
