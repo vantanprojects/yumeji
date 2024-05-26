9c43db8a9c091727eb84589ff889dc53e05b7f33
柳橋莉空 <yagihashirikuu@yagihashirikuunoMacBook-Air.local>
2024-05-26 20:13:08 +0900

    feat/mainpageを消して、taskアプリケーションを作成

--- project/code/task/apps.py
+++ project/code/task/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class MainpageConfig(AppConfig):
+class TaskConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'mainpage'
+    name = 'task'
