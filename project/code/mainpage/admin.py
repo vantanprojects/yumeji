6fff9778db930750a82377d959e7bb2f6e459367
TERUZ-VTAT <terunori.kato0911@gmail.com>
2024-05-26 19:48:34 +0900

    adminサイトからDBを閲覧できるように変更

--- project/code/mainpage/admin.py
+++ project/code/mainpage/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from mainpage.models import Project, ProjectMember, ChatMessage, File, Task, Tag
 
 # Register your models here.
+admin.site.register(Project)
+admin.site.register(ProjectMember)
+admin.site.register(ChatMessage)
+admin.site.register(File)
+admin.site.register(Task)
+admin.site.register(Tag)
